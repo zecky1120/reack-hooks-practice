4bfc99d3f898af73b0e53c6b6be31387f40e2953
zecky1120 <takahide1120@gmail.com>
2025-07-29 20:58:28 +0900

    add: カスタムフック実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import useCustomHook from "./hooks/useCustomHook";
 
 function App() {
-  const [fName, setFname] = useState("");
-  const [lName, setLname] = useState("");
-  const fullName = fName + lName;
+  const [fName, setFname] = useCustomHook("");
+  const [lName, setLname] = useCustomHook("");
+  const fullName = fName + " " + lName;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,21 +16,13 @@ function App() {
         <div className="">
           <label>
             苗字
-            <input
-              type="text"
-              value={fName}
-              onChange={(e) => setFname(e.target.value)}
-            ></input>
+            <input type="text" {...setFname}></input>
           </label>
         </div>
         <div className="">
           <label>
             名前
-            <input
-              type="text"
-              value={lName}
-              onChange={(e) => setLname(e.target.value)}
-            ></input>
+            <input type="text" {...setLname}></input>
           </label>
         </div>
         <div className="">
--- src/hooks/useCustomHook.js
+++ src/hooks/useCustomHook.js
@@ -4,7 +4,7 @@ export default function useCustomHook(initialName) {
   const [name, setName] = useState(initialName);
   const bind = {
     value: name,
-    onchange: (e) => setName(e.target.value),
+    onChange: (e) => setName(e.target.value),
   };
   return [name, bind];
 }
